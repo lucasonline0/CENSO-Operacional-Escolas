76d1d93784767b675d19c69087cd4179fc8402fb
Lucas <129661036+lucasonline0@users.noreply.github.com>
2026-05-28 08:20:17 -0300

    feat: view 004 frente 1
    Co-Authored-By: Pedro Arthur <84817297+pedrofreir@users.noreply.github.com>

--- infra/init.sql
+++ infra/init.sql
@@ -334,3 +334,35 @@ CROSS JOIN LATERAL (
         ('Ciências Humanas',   lower(cd.data->>'possui_coord_area_humanas') IN ('sim', 'true', 't', '1'), 3),
         ('Ciências da Natureza', lower(cd.data->>'possui_coord_area_natureza') IN ('sim', 'true', 't', '1'), 4)
 ) AS v(area, possui, ordem);
+
+CREATE OR REPLACE VIEW vw_censo_quadro_pessoal AS
+SELECT
+    b.school_id,
+    b.codigo_inep,
+    b.nome_escola,
+    b.dre,
+    b.municipio,
+    b.zona,
+    b.year,
+    b.status,
+    b.census_id,
+    CASE WHEN cr.data->>'qtd_professores_efetivos' ~ '^-?[0-9]+(\.[0-9]+)?$'
+         THEN (cr.data->>'qtd_professores_efetivos')::numeric
+         ELSE 0 END AS qtd_professores_efetivos,
+    CASE WHEN cr.data->>'qtd_professores_temporarios' ~ '^-?[0-9]+(\.[0-9]+)?$'
+         THEN (cr.data->>'qtd_professores_temporarios')::numeric
+         ELSE 0 END AS qtd_professores_temporarios,
+    CASE WHEN cr.data->>'qtd_servidores_administrativos' ~ '^-?[0-9]+(\.[0-9]+)?$'
+         THEN (cr.data->>'qtd_servidores_administrativos')::numeric
+         ELSE 0 END AS qtd_servidores_administrativos,
+    CASE WHEN cr.data->>'qtd_professor_readaptado' ~ '^-?[0-9]+(\.[0-9]+)?$'
+         THEN (cr.data->>'qtd_professor_readaptado')::numeric
+         ELSE 0 END AS qtd_professor_readaptado,
+    (CASE WHEN cr.data->>'qtd_professores_efetivos' ~ '^-?[0-9]+(\.[0-9]+)?$'
+           THEN (cr.data->>'qtd_professores_efetivos')::numeric
+           ELSE 0 END +
+     CASE WHEN cr.data->>'qtd_professores_temporarios' ~ '^-?[0-9]+(\.[0-9]+)?$'
+           THEN (cr.data->>'qtd_professores_temporarios')::numeric
+           ELSE 0 END)::numeric AS total_professores
+FROM vw_censo_base b
+JOIN census_responses cr ON cr.id = b.census_id;
